cae50783a14d1b3c3cb267354812abfc4e476977
Nick Russo <nicholasjamesrusso@gmail.com>
2017-07-17 15:35:44 -0700

    lexer: add NextToken and newToken
    Signed-off-by: Nick Russo <nicholasjamesrusso@gmail.com>

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -1,5 +1,7 @@
 package lexer
 
+import "github.com/nijaru/sub/token"
+
 type Lexer struct {
 	input        string
 	position     int
@@ -21,4 +23,36 @@ func (l *Lexer) readChar() {
 	}
 	l.position = l.readPosition
 	l.readPosition += 1
+}
+
+func (l *Lexer) NextToken() token.Token {
+	var t token.Token
+
+	switch l.ch {
+	case '=':
+		t = newToken(token.ASSIGN, l.ch)
+	case ';':
+		t = newToken(token.SEMICOLON, l.ch)
+	case '(':
+		t = newToken(token.LPAREN, l.ch)
+	case ')':
+		t = newToken(token.RPAREN, l.ch)
+	case ',':
+		t = newToken(token.COMMA, l.ch)
+	case '+':
+		t = newToken(token.PLUS, l.ch)
+	case '{':
+		t = newToken(token.LBRACE, l.ch)
+	case '}':
+		t = newToken(token.RBRACE, l.ch)
+	case 0:
+		t.Literal = ""
+		t.Type = token.EOF
+	}
+	l.readChar()
+	return t
+}
+
+func newToken(tokenType token.TokenType, ch byte) token.Token {
+	return token.Token{Type: tokenType, Literal: string(ch)}
 }
